0a16170ad63ff2c43370bd07b370d0d192ffc1c7
farhan-sw <farhansw.off@gmail.com>
2024-07-18 20:45:02 +0700

    feat: add joystick launch

---
+++ nebula-ws/README.md
@@ -0,0 +1,13 @@
+
+## LOG
+
+### 2024-Jul-18
+- Install gamepad library
+```bash
+sudo apt install joystick jstest-gtk evtest
+```
+
+using reference [gamepad](https://articulatedrobotics.xyz/tutorials/mobile-robot/applications/teleop/)
+
+### References
+- [1] [Gamepad by piborg](https://github.com/piborg/Gamepad)
---
+++ nebula-ws/build/nebula_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/farhan-sw/Documents/Github/nebula/nebula-ws/src/nebula_bringup
+# Build directory: /home/farhan-sw/Documents/Github/nebula/nebula-ws/build/nebula_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
