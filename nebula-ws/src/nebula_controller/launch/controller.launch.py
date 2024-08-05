0f3f0f9810476d1dfd1a2d08bc73579c8c0e76b0
farhan-sw <farhansw.off@gmail.com>
2024-08-05 13:52:22 +0700

    feat: add works with alternate omni controller

--- nebula-ws/src/nebula_controller/launch/controller.launch.py
+++ nebula-ws/src/nebula_controller/launch/controller.launch.py
@@ -1,16 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import AndSubstitution, NotSubstitution
 
 def generate_launch_description():
     
-    use_custom_controller_arg = DeclareLaunchArgument(
-        'use_custom_controller',
+    use_simple_controller_arg = DeclareLaunchArgument(
+        'use_simple_controller',
         default_value='false',
         description='Use custom controller (true) or default controller (false)',
+    )
+    
+    use_alternate_omni_controller_arg = DeclareLaunchArgument(
+        'use_alternate_omni_controller',
+        default_value='true',
+        description='Use custom omnidirectional controller (true) or default omnidirectional controller (false)',
     )
     
     wheel_radius_arg = DeclareLaunchArgument(
@@ -25,7 +32,8 @@ def generate_launch_description():
         description='Separation between the wheels',
     )
     
-    use_custom_controller = LaunchConfiguration('use_custom_controller')
+    use_alternate_omni_controller = LaunchConfiguration('use_alternate_omni_controller')
+    use_simple_controller = LaunchConfiguration('use_simple_controller')
     wheel_radius = LaunchConfiguration('wheel_radius')
     wheel_separation = LaunchConfiguration('wheel_separation')
     
@@ -35,37 +43,69 @@ def generate_launch_description():
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"]
     )
 
-    omnidirectional_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["omni_wheel_controller", "--controller-manager", "/controller_manager"],
-        condition=UnlessCondition(use_custom_controller)
+    # Omni Controller
+    omni_controller = GroupAction(
+        condition=UnlessCondition(use_simple_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["omni_wheel_controller", 
+                           "--controller-manager", 
+                           "/controller_manager"],
+                condition=UnlessCondition(use_alternate_omni_controller)
+            ),
+            
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "omnidirectional_controller",
+                    "--controller-manager", 
+                    "/controller_manager",
+                ],
+                condition=IfCondition(use_alternate_omni_controller)
+            )
+        ]
+    )
+   
+    
+
+    
+    # Simple controller
+    simple_controller = GroupAction(
+        condition=IfCondition(use_simple_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["simple_velocity_controller", "--controller-manager", "/controller_manager"],
+            ),
+            Node(
+                package="nebula_controller",
+                executable="controller_3wd_omni_node",
+                name="controller_3wd_omni_node",
+                parameters=[
+                    {"wheel_radius": wheel_radius},
+                    {"wheel_separation": wheel_separation},
+                ],
+            ),
+            
+        ]
     )
     
-    simple_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["simple_velocity_controller", "--controller-manager", "/controller_manager"],
-        condition=IfCondition(use_custom_controller)
-    )
-    
-    simple_controller_cpp = Node(
-        package="nebula_controller",
-        executable="controller_3wd_omni_node",
-        name="controller_3wd_omni_node",
-        parameters=[
-            {"wheel_radius": wheel_radius},
-            {"wheel_separation": wheel_separation},
-        ],
-        condition=IfCondition(use_custom_controller)
-    )
     
     return LaunchDescription([
-        use_custom_controller_arg,
+        use_alternate_omni_controller_arg,
+        use_simple_controller_arg,
+        
         wheel_radius_arg,
         wheel_separation_arg,
+        
         joint_state_broadcaster_spawner,
-        omnidirectional_controller_spawner,
-        simple_controller_spawner,
-        simple_controller_cpp,
+        
+        simple_controller,
+        omni_controller,
+        
+        
     ])
